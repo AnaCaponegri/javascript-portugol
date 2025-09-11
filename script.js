2697ff2f506a43f557e2afbabed3f07301d28731
AnaCaponegri <ana_caponegri@estudante.sc.senai.br>
2025-09-11 19:20:58 -0300

    Variáveis

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Ana Luisa";
+let idade = 19;
+let cidade = "Itatiba - SP";
+let altura = "1.73";
+let estudante = true;
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Cidade: " + cidade);
+console.log("Altura: " + altura);
+console.log("Estudante: " + estudante);
