d5ea7435800abb4b0e88e40c4ff33cca71e5b23b
AnaCaponegri <ana_caponegri@estudante.sc.senai.br>
2025-09-11 19:39:19 -0300

    Exercício Prático Número 1

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let texto = "Olá Mundo!";
-let numero = 42;
-let pi = 3.14;
-let booleano = true;
-let nulo = null;
-let indefinido = undefined;
+let nome = "Ana Luisa";
+let idade = 19;
+let altura = 1.70;
+let estudante = true;
+let endereco = "Rua Raulina Bauer, 271";
 
-console.log(typeof texto); //string
-console.log(typeof numero); //number
-console.log(typeof pi); //number
-console.log(typeof booleano); //boolean
-console.log(typeof nulo); //object (isso é um bug do JavaScript)
-console.log(typeof indefinido); //undefined
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Altura: ${altura}`);
+console.log(`Estudante: ${estudante}`);
+console.log(`Endereço: ${endereco}`);
