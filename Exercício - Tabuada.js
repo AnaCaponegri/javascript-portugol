b3e8b2c8bd683f1b9baf6c169937173db8433cf2
AnaCaponegri <ana_caponegri@estudante.sc.senai.br>
2025-09-15 20:30:42 -0300

    Exercício 03

--- Exercício - Tabuada.js
+++ Exercício - Tabuada.js
@@ -4,5 +4,5 @@ console.log('=== Tabuada do ${numero} ===');
 
 for (let i = 1; i <= 10; i++) {
     let resultado = numero * i;
-    console.log('${numero} x ${i} = ${resultado}');
+    console.log(`${numero} x ${i} = ${resultado}`);
 }
