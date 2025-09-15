221eccb1ef4cc8b048b1efd7de764ae3d7549e1f
AnaCaponegri <ana_caponegri@estudante.sc.senai.br>
2025-09-15 20:45:32 -0300

    Função

---
+++ Função.js
@@ -0,0 +1,29 @@
+/*
+funcao en Portugol
+
+funcao real calculadoraMedia(real n1, real n2, real n3)
+    real media
+    media = (n1 + n2 + n3) / 3
+    retorne media
+fimfuncao
+*/
+
+function soma(n1, n2) {
+return (n1 + n2);
+}
+
+function subtracao(n1, n2) {
+return (n1 - n2);
+}
+
+function multiplicacao(n1, n2) {
+return (n1 * n2);
+}
+
+function divisao(n1, n2) {
+return (n1 / n2);
+}
+
+let resultado = soma(7, 8);
+
+console.log(`A soma é: ${resultado}`); 
